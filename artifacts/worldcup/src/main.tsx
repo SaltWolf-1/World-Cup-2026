a416995f696cba72ae65729e724b0fb40ecc06f4
SaltWolf-1 <52sk6jnggy@privaterelay.appleid.com>
2026-06-17 14:42:14 +0100

    Update main.tsx

--- artifacts/worldcup/src/main.tsx
+++ artifacts/worldcup/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "@workspace/api-client-react";
+
+setBaseUrl("https://workspaceapi-server-production-b604.up.railway.app");
+
 createRoot(document.getElementById("root")!).render(<App />);
